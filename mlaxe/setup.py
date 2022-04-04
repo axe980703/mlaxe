c4616b6718421fe70ab581336c6f7e66d58b8464
axe980703 <axe980703@gmail.com>
2022-04-05 03:07:57 +0500

    added setup.py, modified reqs.txt

---
+++ mlaxe/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+
+setup(
+    name="mlaxe",
+    version="0.0.1",
+    author="axe980703",
+    description="Machine Learning library with visualization support",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/axe980703/mlaxe",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8.6",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
