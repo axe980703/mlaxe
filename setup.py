ab18cdde9ffe56a75dafc895a7cc55f32d4a738d
axe980703 <axe980703@gmail.com>
2022-04-05 21:27:40 +0500

    commit after successfull uploading to pypi

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
+requirements = ["numpy>=1.19.0", "matplotlib>=3.2.0"]
 
 setup(
     name="mlaxe",
-    version="0.0.1",
+    version="0.0.13",
     author="axe980703",
     description="Machine Learning library with visualization support",
     long_description=readme,
@@ -15,7 +16,8 @@ setup(
     url="https://github.com/axe980703/mlaxe",
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.8.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
+    install_requires=requirements
 )
